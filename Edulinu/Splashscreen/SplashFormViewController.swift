3b8968f0b0177e598f5d0dc6e536ed904bff2632
Laurens K <l.kropf@icloud.com>
2019-10-17 15:27:52 +0200

    splash screen form design changes

--- Edulinu/Splashscreen/SplashFormViewController.swift
+++ Edulinu/Splashscreen/SplashFormViewController.swift
@@ -12,57 +12,7 @@ import Eureka
 class SplashFormViewController: FormViewController {
     
     @IBAction func saveButton(_ sender: Any) {
-        
-        let formValues = form.values()
-        
-        if formValues["firstName"] as? String == nil || formValues["lastName"] as? String == nil || formValues["pmsClass"] as? String == nil {
-            
-            let alert = UIAlertController(title: "Fehler", message: "Bitte fülle alle Felder korrekt aus", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alert, animated: true)
-            
-        } else {
-            
-            let firstName = formValues["firstName"] as! String
-            let lastName = formValues["lastName"] as! String
-            let pmsClass = formValues["pmsClass"] as! String
-            
-            if !firstName.isEmpty && !lastName.isEmpty && !pmsClass.isEmpty && pmsClass != "Bitte auswählen" {
-                
-                if pmsClass == "Ich bin ein Elternteil" {
-                    edulinuLocalUserSettings.set("parent", forKey: Keys.ElusUserRole)
-                    edulinuLocalUserSettings.set("noClass", forKey: Keys.ElusClass)
-                } else if pmsClass == "Ich bin Lehrer/in" {
-                    edulinuLocalUserSettings.set("teacher", forKey: Keys.ElusUserRole)
-                    edulinuLocalUserSettings.set("noClass", forKey: Keys.ElusClass)
-                } else {
-                    edulinuLocalUserSettings.set("student", forKey: Keys.ElusUserRole)
-                    edulinuLocalUserSettings.set(pmsClass, forKey: Keys.ElusClass)
-                }
-                
-                edulinuLocalUserSettings.set(firstName, forKey: Keys.ElusFirstName)
-                edulinuLocalUserSettings.set(lastName, forKey: Keys.ElusLastName)
-                edulinuLocalUserSettings.set(Date(), forKey: Keys.ElusLastUpdateDate)
-                edulinuLocalUserSettings.set([""], forKey: Keys.ElusFavouriteTeachers)
-                
-                let storyboard = UIStoryboard(name: "Splashscreen", bundle: nil)
-                let splashGetStartedViewController = storyboard.instantiateViewController(withIdentifier: "SplashNotificationsViewController") as! SplashNotificationsViewController
-                splashGetStartedViewController.modalTransitionStyle = .crossDissolve
-                splashGetStartedViewController.modalPresentationStyle = .fullScreen
-                presentWithSlide(splashGetStartedViewController)
-                
-            } else {
-                
-                let alert = UIAlertController(title: "Fehler", message: "Bitte fülle alle Felder korrekt aus", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                self.present(alert, animated: true)
-                
-            }
-            
-        }
-        
-        
-        
+        saveForm()
     }
     
     override func viewDidLoad() {
@@ -116,6 +66,87 @@ class SplashFormViewController: FormViewController {
                     cell.textLabel!.textColor = .white
                 }
                 
+                
+                
         }
+            
+            +++ Section()
+            
+            <<< ButtonRow(){ row in
+                row.title = "Sichern"
+                row.tag = "saveFormRowButton"
+                row.onCellSelection(self.saveFormRowButton)
+                }
+        
+            +++ Section()
+            
+            <<< ButtonRow(){ row in
+                row.title = "Wofür werden diese Daten genutzt?"
+                row.tag = "privacyInfoButton"
+                row.onCellSelection(self.privacyInfo)
+                }
+    }
+    
+    func saveFormRowButton(cell: ButtonCellOf<String>, row: ButtonRow) {
+        saveForm()
+    }
+    
+    func saveForm() {
+        
+        let formValues = form.values()
+        
+        if formValues["firstName"] as? String == nil || formValues["lastName"] as? String == nil || formValues["pmsClass"] as? String == nil {
+            
+            let alert = UIAlertController(title: "Hinweis", message: "Bitte fülle alle Felder korrekt aus", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            
+        } else {
+            
+            let firstName = formValues["firstName"] as! String
+            let lastName = formValues["lastName"] as! String
+            let pmsClass = formValues["pmsClass"] as! String
+            
+            if !firstName.isEmpty && !lastName.isEmpty && !pmsClass.isEmpty && pmsClass != "Bitte auswählen" {
+                
+                if pmsClass == "Ich bin ein Elternteil" {
+                    edulinuLocalUserSettings.set("parent", forKey: Keys.ElusUserRole)
+                    edulinuLocalUserSettings.set("noClass", forKey: Keys.ElusClass)
+                } else if pmsClass == "Ich bin Lehrer/in" {
+                    edulinuLocalUserSettings.set("teacher", forKey: Keys.ElusUserRole)
+                    edulinuLocalUserSettings.set("noClass", forKey: Keys.ElusClass)
+                } else {
+                    edulinuLocalUserSettings.set("student", forKey: Keys.ElusUserRole)
+                    edulinuLocalUserSettings.set(pmsClass, forKey: Keys.ElusClass)
+                }
+                
+                edulinuLocalUserSettings.set(firstName, forKey: Keys.ElusFirstName)
+                edulinuLocalUserSettings.set(lastName, forKey: Keys.ElusLastName)
+                edulinuLocalUserSettings.set(Date(), forKey: Keys.ElusLastUpdateDate)
+                edulinuLocalUserSettings.set([""], forKey: Keys.ElusFavouriteTeachers)
+                
+                let storyboard = UIStoryboard(name: "Splashscreen", bundle: nil)
+                let splashGetStartedViewController = storyboard.instantiateViewController(withIdentifier: "SplashNotificationsViewController") as! SplashNotificationsViewController
+                splashGetStartedViewController.modalTransitionStyle = .crossDissolve
+                splashGetStartedViewController.modalPresentationStyle = .fullScreen
+                presentWithSlide(splashGetStartedViewController)
+                
+            } else {
+                
+                let alert = UIAlertController(title: "Hinweis", message: "Bitte fülle alle Felder korrekt aus", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert, animated: true)
+                
+            }
+            
+        }
+    }
+    
+    func privacyInfo(cell: ButtonCellOf<String>, row: ButtonRow) {
+        
+        let alert = UIAlertController(title: "Wofür werden meine Daten genutzt", message: "Dein Vor- und Nachname wird dazu verwendet, dir in dieser App persönliche Nachrichten anzeigen zu können. Außerdem können wir dir personalisierte Push-Benachrichtigungen senden.\nDeine Klasse bzw. den Nutzerstatus benötigen wir, um die App für deinen Nutzerstatus anzupassen.\nWeitere Fragen?\nSchreibe uns ein E-Mail:\ndevelopment@edulinu.com", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+        
     }
 }
